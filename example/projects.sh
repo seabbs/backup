526f5ec798c72c2e49cbe2e15a91a06869be297d
Sam Abbott <sam.abbott@bristol.ac.uk>
2019-11-20 12:17:58 +0000

    added example projects.sh

---
+++ example/projects.sh
@@ -0,0 +1,7 @@
+# Declare an array of projects
+declare -a projects=("idmodelr"
+                     "getTBinR"
+                     "prettypublisher")
+
+# Declare the base address for projects
+base_url="https://github.com/seabbs"
